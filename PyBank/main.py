e99196d41baca4947cbcc451af1287f355ba641a
Taiwo Onitiri <taiwo@beyondbordersinvestment.com>
2019-08-02 18:32:26 -0500

    updated code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,33 @@
 # import os module & CSV
 import os
 import csv
-path = '../Resources/budget_data.csv'
+path = 'C:\\Users\\taiwo\\OneDrive\\Desktop\\phyton-challenge\\PyBank\\Resources\\budget_data.csv'
+ 
+# open file with CSV reader
+with open (path , 'r') as work1_csv_file:
+    csv_reader = csv.reader(work1_csv_file)
+    next(csv_reader)
+    for row in csv_reader:
+        #print(', '.join(row))
+        print(row[0])
 
-csvpath = os.path.join('..', 'Resources', 'budget_data.csv')
+        #assigning variable type to columns
+        months = str(row[0])
+        profit_loss = float(row[1])
 
-with open(csvpath, 'r') as file_handler:
-    lines = file_handler.read()
-    print(lines)
-    print(type(lines))
+        #total number of months
+        #print(months).value_count()
+        # Net total profit/losses
+        #print(len('months'))
+        print(profit_loss).value_count
+
+    # def average(numbers):
+    # length = len(numbers)
+    # total = 0.0
+    # for number in numbers:
+    #     total += number
+    # return total / length
+          
+
+        
+
