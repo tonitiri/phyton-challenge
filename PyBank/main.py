082a64c7486a6f9373f6f97f82e1700d76834924
Taiwo Onitiri <taiwo@beyondbordersinvestment.com>
2019-08-03 16:34:48 -0500

    homework code change

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,31 +2,61 @@
 import os
 import csv
 path = 'C:\\Users\\taiwo\\OneDrive\\Desktop\\phyton-challenge\\PyBank\\Resources\\budget_data.csv'
- 
+
+net_profit=0
+sum_rows=0
+average=0
+greatest_increase=0
+budget_data=0
+
 # open file with CSV reader
 with open (path , 'r') as work1_csv_file:
     csv_reader = csv.reader(work1_csv_file)
     next(csv_reader)
+
+
     for row in csv_reader:
-        #print(', '.join(row))
-        print(row[0])
+    
+         x = int(row[1])
 
-        #assigning variable type to columns
-        months = str(row[0])
-        profit_loss = float(row[1])
+         net_profit = x + net_profit
+         sum_rows = sum_rows + 1
+         average = net_profit/sum_rows
+         
+         
+#Net Total Profit
+print(net_profit) 
+#Total number of months
+print(sum_rows)
+#Average
+print(average)
 
-        #total number of months
-        #print(months).value_count()
-        # Net total profit/losses
-        #print(len('months'))
-        print(profit_loss).value_count
 
-    # def average(numbers):
-    # length = len(numbers)
-    # total = 0.0
-    # for number in numbers:
-    #     total += number
-    # return total / length
+    #     #print(', '.join(row))
+    #     #print(row[1])
+    #     total_rows = total_rows + 1
+
+    #     #1. Total Number of Months
+    #     print(total_rows)
+
+        # budget_data_list.extend([row[1]])
+        # budget_data_list.extend([x])
+    #     print(type(budget_data_list))
+    #     # print(type(x))
+
+#for n in range(x)
+    
+        
+        
+
+
+
+
+
+
+        
+
+        
           
 
         
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,12 +1,29 @@
 # import os module & CSV
 import os
 import csv
-path = 'C:\\Users\\taiwo\\OneDrive\\Desktop\\Working\\02-Homework\\03-Python\\Instructions\\PyPoll\\Resources\\election_data.csv'
- 
+path = 'C:\\Users\\taiwo\\OneDrive\\Desktop\\phyton-challenge\\PyPoll\\Resources\\election_data.csv'
+
+sum_of_rows=0
+x=" "
+y=" "
+z=" "
 # open file with CSV reader
 with open (path , 'r') as work2_csv_file:
     csv_reader = csv.reader(work2_csv_file)
     next(csv_reader)
     for row in csv_reader:
-        #print(', '.join(row)).headers
-        #print(row[0])
+        x = str(row[0])
+        y = str(row[1])
+        z = str(row[2])
+
+        sum_of_rows = sum_of_rows + 1
+
+#Total number of votes
+print(sum_of_rows)
+#List of candidates
+print(set(row[2]))
+        
+
+
+        #print(', '.join(row))
+        
